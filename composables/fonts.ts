49a4dfc743e4b84b44832105033f664c56e4a583
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-03-16 12:23:36 +0200

    fix: underline text and set the new fonts

--- composables/fonts.ts
+++ composables/fonts.ts
@@ -2,7 +2,7 @@ const fontsMetaList = [
   {
     rel: 'preload',
     type: 'font/woff2',
-    href: '/fonts/SuisseIntlMono.woff2',
+    href: '/fonts/SuisseIntl-Regular-WebM.woff2',
     crossorigin: 'anonymous',
     as: 'font',
   },
@@ -10,7 +10,31 @@ const fontsMetaList = [
   {
     rel: 'preload',
     type: 'font/woff2',
-    href: '/fonts/SuisseIntlRegular.woff2',
+    href: '/fonts/SuisseIntl-Regular-WebS.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/SuisseIntl-Regular-WebXL.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/SuisseIntlMono-Regular-WebS.woff2',
+    crossorigin: 'anonymous',
+    as: 'font',
+  },
+
+  {
+    rel: 'preload',
+    type: 'font/woff2',
+    href: '/fonts/SuisseIntlMono-Regular-WebXL.woff2',
     crossorigin: 'anonymous',
     as: 'font',
   },
